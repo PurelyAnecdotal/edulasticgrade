f73d90901982e29af2c58ffae233cbb421d2d305
Nonexistent-Name <58897346+Nonexistent-Name@users.noreply.github.com>
2023-06-22 09:39:06 -0700

    Fix loading and update page

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-fetch('https://cdn.jsdelivr.net/gh/nonexistent-name/edulasticgrade/script.min.js').then(res => res.text()).then(js => eval(js));
+import('https://cdn.jsdelivr.net/gh/nonexistent-name/edulasticgrade/script.min.js');
