7f2565fda32f6dc51391590eb72738b1fe2a6725
Nonexistent-Name <58897346+Nonexistent-Name@users.noreply.github.com>
2023-08-25 10:45:52 -0700

    Simplify

--- script.js
+++ script.js
@@ -7,16 +7,14 @@ iframe.remove();
 
 const headers = new Headers({
     Authorization: localStorage.getItem(
-        JSON.parse(localStorage.getItem('tokens'))[0]
+        JSON.parse(localStorage.getItem('tokens'))[0],
     ),
 });
 
-const fetchAPI = async url =>
+const fetchAPI = async (url) =>
     (
         await (
-            await fetch(`https://app.edulastic.com/api/${url}`, {
-                headers,
-            })
+            await fetch(`https://app.edulastic.com/api/${url}`, { headers })
         ).json()
     ).result;
 
@@ -24,25 +22,23 @@ const popup = open('about:blank');
 
 const popupHTML = await (
     await fetch(
-        'https://cdn.jsdelivr.net/gh/nonexistent-name/edulasticgrade/popup.html'
+        'https://cdn.jsdelivr.net/gh/nonexistent-name/edulasticgrade/popup.html',
     )
 ).text();
 
 popup.document.write(popupHTML);
 popup.document.close();
 
-const groupId = location.pathname.slice(12, 36);
-const testId = location.pathname.slice(42, 66);
-const reportId = location.pathname.slice(-24);
+const path = location.pathname.split('/');
 
 const test = await fetchAPI(
-    `test/${testId}/minimal?testActivityId=${reportId}`
+    `test/${path[5]}/minimal?testActivityId=${path[7]}`,
 );
 
 popup.document.getElementById('title').innerText = test.title;
 
 const report = await fetchAPI(
-    `test-activity/${reportId}/report?groupId=${groupId}`
+    `test-activity/${path[7]}/report?groupId=${path[3]}`,
 );
 
 const total = report.questionActivities.length;
@@ -60,12 +56,12 @@ popup.document.getElementById('p-inner').style.width = `${
 const questionList = popup.document.getElementById('questions');
 
 const parser = new DOMParser();
-const parse = html => parser.parseFromString(html, 'text/html');
+const parse = (html) => parser.parseFromString(html, 'text/html');
 
 test.itemGroups[0].items
-    .filter(item => item.rows[0].widgets[0].type === 'multipleChoice')
-    .map(item => item.data.questions[0])
-    .forEach(question => {
+    .filter((item) => item.rows[0].widgets[0].type === 'multipleChoice')
+    .map((item) => item.data.questions[0])
+    .forEach((question) => {
         const parsed = parse(question.stimulus).body;
 
         const li = document.createElement('li');
@@ -94,8 +90,8 @@ test.itemGroups[0].items
         li.appendChild(optionList);
 
         question.options
-            .map(option => option.label)
-            .forEach(label => {
+            .map((option) => option.label)
+            .forEach((label) => {
                 const option = document.createElement('li');
                 option.innerText = `${parse(label).body.innerText.trim()} `;
 
